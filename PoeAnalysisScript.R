45fab5cfb15162bbc2d17867cd37a3749ae1c047
Daniel López <dlopez@dani.idk.local>
2015-02-10 18:00:13 +0100

    first word cloud

--- PoeAnalysisScript.R
+++ PoeAnalysisScript.R
@@ -1,25 +1,8 @@
+install.packages("wordcloud")
+library(tm)
+library(wordcloud)
 
-#Function to download the data -------------------------------------
-read_data <- function(fileName, source_url) {  
-  if (!file.exists(fileName)) {
-    download.file(source_url, destfile = fileName, method = "curl")
-  }
-  text <- readLines(fileName,encoding="UTF-8")
-  text <- paste(text, collapse=" ")
-}
-#-------------------------------------------------------------------
-#Function to parse tales -------------------------------------
-parse_tales <- function(text, titles) {  
-  tales <- vector()
-  for(title in titles){
-    spl <- unlist(strsplit(text,title))
-    tales <- c(tales,spl[1])
-    text <- spl[2]
-  }
-  tales[-1] #drop first element
-}
-#-------------------------------------------------------------------
-
+stopWords <- stopwords("en")
 
 urlV <- c("https://www.gutenberg.org/cache/epub/2147/pg2147.txt",
           "https://www.gutenberg.org/cache/epub/2148/pg2148.txt",
@@ -41,4 +24,37 @@ tittlesV1 <- c("THE UNPARALLELED ADVENTURES OF ONE HANS PFAAL",
                "End of Project Gutenberg")
 
 splittingV1 <- parse_tales(poev1text,tittlesV1)
-substr(splittingV1[1], 1, 20)
+textNoPunct <- gsub("[[:blank:][:punct:]+]", " ", splittingV1[1])
+textlow <- tolower(textNoPunct)
+'%nin%' <- Negate('%in%')
+
+textWords <- unlist(strsplit(textlow, " "))
+
+textWords <- textWords[textWords %nin% c(stopWords,"")]
+
+wordcloud(textWords,min.freq=2,max.words=70, random.order=T, rot.per=.15,  vfont=c("sans serif","plain"))
+
+substr(splittingV1[1], 1, 2000)
+substr(textNoPunct, 1, 2000)
+
+##FUNCTIONS
+
+#Function to download the data -------------------------------------------
+read_data <- function(fileName, source_url) {  
+  if (!file.exists(fileName)) {
+    download.file(source_url, destfile = fileName, method = "curl")
+  }
+  text <- readLines(fileName,encoding="UTF-8")
+  text <- paste(text, collapse=" ")
+}
+
+#Function to parse tales -------------------------------------------------
+parse_tales <- function(text, titles) {  
+  tales <- vector()
+  for(title in titles){
+    spl <- unlist(strsplit(text,title))
+    tales <- c(tales,spl[1])
+    text <- spl[2]
+  }
+  tales[-1] #drop first element
+}
